7a6d2ea491bd67412cecbd76c53318a5fc111e3e
lukejacksonn <lukejacksonn@gmail.com>
2019-07-09 20:23:21 +0100

    Implements init and bin

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const http = require('http');
-const WebSocket = require('ws');
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const http = require("http");
+const ws = require("ws");
+const cproc = require("child_process");
 
 // ----------------------------------
 // Generate map of all known mimetypes
 // ----------------------------------
 
-const mime = Object.entries(require('./types.json')).reduce(
+const mime = Object.entries(require("./types.json")).reduce(
   (all, [type, exts]) =>
     Object.assign(all, ...exts.map(ext => ({ [ext]: type }))),
   {}
@@ -20,35 +21,30 @@ const mime = Object.entries(require('./types.json')).reduce(
 // Parse arguments from the command line
 // ----------------------------------
 
-const defaults = {
-  root: '.',
-  fallback: 'index.html',
-  port: 8080,
-  subPort: 1337,
-  reloadPort: 5000,
-  browser: 'yes'
-};
+const args = process.argv.slice(2).filter(x => !~x.indexOf("--"));
 
-const input = process.argv.slice(2).join(' ');
-// Extract positional arguments
-const args = input
-  .replace(/--([^\s]*)\s[^\s]*(\s)?/g, '')
-  .trim()
-  .split(' ');
-// Extract named arguments
-const named = (input.match(/--([^\s]*)\s[^\s]*/g, '') || []).reduce((a, b) => {
-  const [key, value] = b.split(' ');
-  return Object.assign(a, { [key.replace('--', '')]: value });
-}, {});
-
-const options = Object.entries(defaults).reduce(
-  (opts, [key, val], i) =>
-    Object.assign(opts, { [key]: named[key] || args[i] || val }),
-  {}
-);
-
-const { root, fallback, port, reloadPort, browser, subPort } = options;
+const root = args[0] || ".";
+const fallback = args[1] || "index.html";
+const port = args[2] || 8080;
+const subPort = args[3] || 1337;
+const reloadPort = args[4] || 5000;
+const init = !!~process.argv.indexOf("--init");
 const cwd = process.cwd();
+
+// ----------------------------------
+// Create an example app if --init
+// ----------------------------------
+
+if (init) {
+  const id = `glu-${Math.random()
+    .toString(16)
+    .replace("0.", "")
+    .slice(5)}`;
+  cproc.execSync(
+    `mkdir ${id} && cp ${__dirname}/example/* ${id} && cd ${id} && glu`
+  );
+  return;
+}
 
 // ----------------------------------
 // Template clientside reload script
@@ -57,13 +53,13 @@ const cwd = process.cwd();
 const glu = body => {
   const socket = new WebSocket(`ws://localhost:${__SUB_PORT__}`);
   const off = () => socket.close();
-  socket.addEventListener('open', () => socket.send(body));
+  socket.addEventListener("open", () => socket.send(body));
 
   const once = () => {
     return new Promise(resolve => {
       let handler = ({ data }) =>
-        socket.removeEventListener('message', handler) || resolve(data);
-      socket.addEventListener('message', handler);
+        socket.removeEventListener("message", handler) || resolve(data);
+      socket.addEventListener("message", handler);
     });
   };
 
@@ -73,7 +69,7 @@ const glu = body => {
 
   return cb => {
     return new Promise(resolve => {
-      socket.addEventListener('close', resolve);
+      socket.addEventListener("close", resolve);
       (async () => {
         for await (const response of getResponse()) cb(response, off);
       })();
@@ -99,29 +95,29 @@ const reloadScript = `
 const sendError = (res, resource, status) => {
   res.writeHead(status);
   res.end();
-  console.log(' \x1b[41m', status, '\x1b[0m', `${resource}`);
+  console.log(" \x1b[41m", status, "\x1b[0m", `${resource}`);
 };
 
 const sendFile = (res, resource, status, file, ext) => {
   res.writeHead(status, {
-    'Content-Type': mime[ext] || 'application/octet-stream',
-    'Access-Control-Allow-Origin': '*'
+    "Content-Type": mime[ext] || "application/octet-stream",
+    "Access-Control-Allow-Origin": "*"
   });
-  res.write(file, 'binary');
+  res.write(file, "binary");
   res.end();
-  console.log(' \x1b[42m', status, '\x1b[0m', `${resource}`);
+  console.log(" \x1b[42m", status, "\x1b[0m", `${resource}`);
 };
 
 const sendMessage = (res, channel, data) => {
   res.write(`event: ${channel}\nid: 0\ndata: ${data}\n`);
-  res.write('\n\n');
+  res.write("\n\n");
 };
 
 const isRouteRequest = uri =>
   uri
-    .split('/')
+    .split("/")
     .pop()
-    .indexOf('.') === -1
+    .indexOf(".") === -1
     ? true
     : false;
 
@@ -130,21 +126,21 @@ const isRouteRequest = uri =>
 // ----------------------------------
 
 http
-  .createServer((request, res) => {
+  .createServer((req, res) => {
     // Open the event stream for live reload
     res.writeHead(200, {
-      Connection: 'keep-alive',
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Access-Control-Allow-Origin': '*'
+      Connection: "keep-alive",
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      "Access-Control-Allow-Origin": "*"
     });
     // Send an initial ack event to stop request pending
-    sendMessage(res, 'connected', 'awaiting change');
+    sendMessage(res, "connected", "awaiting change");
     // Send a ping event every minute to prevent console errors
-    setInterval(sendMessage, 60000, res, 'ping', 'still waiting');
+    setInterval(sendMessage, 60000, res, "ping", "still waiting");
     // Watch the target directory for changes and trigger reload
     fs.watch(path.join(cwd, root), { recursive: true }, () =>
-      sendMessage(res, 'message', 'reloading page')
+      sendMessage(res, "message", "reloading page")
     );
   })
   .listen(parseInt(reloadPort, 10));
@@ -153,13 +149,13 @@ http
 // Start command running server
 // ----------------------------------
 
-new WebSocket.Server({ port: subPort }).on('connection', socket => {
-  socket.on('message', body => {
-    const [cmd, ...args] = body.split(' ');
-    const proc = require('child_process').spawn(cmd, args);
-    proc.stdout.on('data', stdout => socket.send(stdout.toString()));
-    proc.on('error', e => socket.send(e.toString()));
-    proc.on('exit', e => socket.close(e.code));
+new ws.Server({ port: subPort }).on("connection", socket => {
+  socket.on("message", body => {
+    const [cmd, ...args] = body.split(" ");
+    const proc = require("child_process").spawn(cmd, args);
+    proc.stdout.on("data", stdout => socket.send(stdout.toString()));
+    proc.on("error", e => socket.send(e.toString()));
+    proc.on("exit", e => socket.close(e.code));
   });
 });
 
@@ -171,16 +167,16 @@ http
   .createServer((req, res) => {
     const pathname = url.parse(req.url).pathname;
     const isRoute = isRouteRequest(pathname);
-    const status = isRoute && pathname !== '/' ? 301 : 200;
+    const status = isRoute && pathname !== "/" ? 301 : 200;
     const resource = isRoute ? `/${fallback}` : decodeURI(pathname);
     const uri = path.join(cwd, root, resource);
-    const ext = uri.replace(/^.*[\.\/\\]/, '').toLowerCase();
-    isRoute && console.log('\n \x1b[44m', 'RELOADING', '\x1b[0m\n');
+    const ext = uri.replace(/^.*[\.\/\\]/, "").toLowerCase();
+    isRoute && console.log("\n \x1b[44m", "RELOADING", "\x1b[0m\n");
     // Check if files exists at the location
     fs.stat(uri, (err, stat) => {
       if (err) return sendError(res, resource, 404);
       // Respond with the contents of the file
-      fs.readFile(uri, 'binary', (err, file) => {
+      fs.readFile(uri, "binary", (err, file) => {
         if (err) return sendError(res, resource, 500);
         if (isRoute) file = reloadScript + file;
         sendFile(res, resource, status, file, ext);
@@ -201,16 +197,6 @@ console.log(` ♻️  Reloading the browser when files under ./${root} change`);
 // Open the page in the default browser
 // ----------------------------------
 
-// const page = `http://localhost:${port}`;
-// const open =
-//   process.platform == "darwin"
-//     ? "open"
-//     : process.platform == "win32"
-//     ? "start"
-//     : "xdg-open";
-
-// browser !== "no" && require("child_process").exec(open + " " + page);
-
-require('child_process').exec(
+cproc.exec(
   `/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --app='http://localhost:${port}'`
 );
