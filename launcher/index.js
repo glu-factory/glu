3bf5fdd8522c8f0429a81d680b74d010f1597415
lukejacksonn <lukejacksonn@gmail.com>
2019-07-22 17:48:44 +0100

    Store and list created apps

--- launcher/index.js
+++ launcher/index.js
@@ -16,11 +16,22 @@ glu('ping google.com')((data, off) => {
   .catch(console.error);
 
 window.launch = async template => {
-  var id = `glu-${Math.random()
+  var id = `glu-${template}-${Math.random()
     .toString(16)
-    .replace('0.', '')
-    .slice(5)}`;
+    .slice(3, 8)}`;
   await glu(`mkdir ${id}`)(console.log);
   await glu(`cp -r ${__dirname}/templates/${template}/. ${id}/`)(console.log);
+
+  let cwd;
+  await glu('pwd')(data => (cwd = data.trim()));
+  await glu(
+    `touch ${__dirname}/created/${`${cwd}/${id}`.replace(/\//g, '->')}`
+  )(console.log);
+
+  await glu(`ls ${__dirname}/created`)(data => {
+    const projects = data.split('\n').map(x => x.replace(/\->/g, '/'));
+    setNode('created')(projects.map(x => `<li>${x}</li>`).join(''));
+  });
+
   glu(`glu ${id}`);
 };
