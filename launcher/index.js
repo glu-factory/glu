afe62d9a32e1fcee118d451b1332841ea7e468eb
lukejacksonn <lukejacksonn@gmail.com>
2019-07-31 16:33:53 +0100

    Remove svg from launcher index and add value to search input

--- launcher/index.js
+++ launcher/index.js
@@ -439,8 +439,8 @@ const App = () => {
               >
                 <input
                   onInput=${e => setSearch(e.target.value)}
-                  type="search"
                   placeholder="Search for projects.."
+                  value=${search}
                 />
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path
